48f5bba9c035f533c0a79a83f83e444cd89d3124
jramlope <jramlope@everis.com>
2021-03-24 13:48:35 +0100

    Versión 1 - GIT

---
+++ src/com/everisgit/AppMain.java
@@ -0,0 +1,18 @@
+package com.everisgit;
+/**
+ * Formación de Git, configurar en mi eclipse Git
+ * @author jramlope
+ *
+ */
+public class AppMain {
+	/**
+	 * Método principal
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hola Basilio");
+		
+	}
+
+}
